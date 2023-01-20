7d6e005c8ca4cc941d19b942504f3a0f6063f229
RevantriPutra <111005437+RevantriPutra@users.noreply.github.com>
2023-01-20 19:35:05 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<body>
+  
+  <div class="login">
+
+    <div class="avatar">
+      <i class="fa fa-user"></i>
+    </div>
+
+    <h2>Login User</h2>
+    <form method="post">
+    <div class="box-login">
+      <i class="fas fa-envelope"></i>
+    <input type="text" placeholder="Kode login" name="kodelogin" id="kodelogin" autocomplete="off" required>
+    </div>
+
+    <div class="box-login">
+      <i class="fas fa-lock"></i>
+      <input type="password" placeholder="Password" id="pswd" name="pswd" required>
+    </div>
+
+    <button type="submit" class="btn-login" name="blogin" id="blogin">Login</button>
+<br><div class="small"><font color="red"><a href="registrasi.php">Registrasi? </a></div></font>   
+    
+  </div>
